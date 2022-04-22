2ba422fc614887249429d50c10829c2388daa602
Kennedy Martins <kennedy.martins@valemobi.com.br>
2022-04-22 16:03:54 -0300

    Fix: alteração no css dos cards

--- dream-house/src/components/Card.jsx
+++ dream-house/src/components/Card.jsx
@@ -10,7 +10,7 @@ function Card(props) {
             <div className="card">
                 <img className="card-img" src={Quarto} alt="Rome" />
                 <div className="card-content">
-                    <div className="text-container">
+                    <div className="card-text-container">
                         <h1 className="card-header">{props.bairro}</h1>
                         <h2 className="card-header">{props.cidade}</h2>
                         <h3 className="card-header">{props.valor}</h3>
