21be177519d22772277b20b952eb41de3b550ed8
Kennedy Martins <kennedy.martins@valemobi.com.br>
2022-05-02 17:26:07 -0300

    Feature: criando componente sidenav, tela dashboard e instalação das dependencias do MUI

--- dream-house/src/App.js
+++ dream-house/src/App.js
@@ -1,14 +1,19 @@
 import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
 import Cadastro from "./pages/Cadastro";
 
 
 function App() {
   return (
     <div className="App">
-       <Home/>
+       {/* <Home/> */}
        {/* <Cadastro/> */}
+       <Dashboard/>
     </div>
   );
 }
 
 export default App;
+
+// instalar o MUI via terminal 
+// Use este comando: npm install @mui/material @emotion/react @emotion/styled
