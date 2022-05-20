1039c32404cfa76f4309063e1ad1bbe2dae12327
unknown <anderson.deitaqu4@gmail.com>
2022-05-20 20:06:29 -0300

    Page Sobre

--- dream-house/src/Routes.jsx
+++ dream-house/src/Routes.jsx
@@ -8,6 +8,7 @@ import CadastroAnuncioDetalhes from "./pages/CadastroDetalhesAnuncio";
 import Locacao from "./pages/Locacao";
 import EditPerfil from "./pages/EditPerfil";
 import MeusAnuncios from "./pages/MeusAnuncios";
+import Sobre from "./pages/Sobre";
 
 
 
@@ -24,6 +25,7 @@ function Rotas() {
                 <Route path="/locacao" exact element={<Locacao/>} />
                 <Route path="/edit" exact element={<EditPerfil/>}/>
                 <Route path="/meus-anuncios" exact element={<MeusAnuncios />} />
+                <Route path="/sobre" exact element={<Sobre/>} />
             </Routes>
         </BrowserRouter>
 
