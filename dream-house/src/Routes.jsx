80966bd689f9cb430412b2962bf370c46d7d2010
Kennedy Martins <kennedy.martins@valemobi.com.br>
2022-05-23 19:49:21 -0300

    Feature: merge branch dev-kennedy

--- dream-house/src/Routes.jsx
+++ dream-house/src/Routes.jsx
@@ -27,7 +27,7 @@ function Rotas() {
                 <Route path="/locacao" exact element={<Locacao/>} />
                 <Route path="/edit" exact element={<EditPerfil/>}/>
                 <Route path="/meus-anuncios" exact element={<MeusAnuncios />} />
-                <Route path="/dashboard" exact element={<Dashboard />} />
+                <Route path="/dashboard" exact element={<Dashboard />} />   
                 <Route path="/sobre" exact element={<Sobre/>} />
                 <Route path="/anuncios" exact element={<Anuncios/>} />
             </Routes>
