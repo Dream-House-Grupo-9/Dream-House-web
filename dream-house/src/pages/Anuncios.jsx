ca4bf8a951e04f39474080030b153b6c0eb1304e
FelipeAmorim720 <felipeamorimreis40@gmail.com>
2022-05-23 17:35:18 -0300

    rela principal

--- dream-house/src/pages/Anuncios.jsx
+++ dream-house/src/pages/Anuncios.jsx
@@ -1,19 +1,40 @@
 import React, { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
-import CardMeusAnuncios from "../components/CardMeusAnuncios";
+import Card from "../components/Card";
 import "../css/anuncios.css";
 import "../Js/ActionModal.js";
 import filter from "../assets/img/filter.png";
 
 function Anuncio(props) {
+    
+    const [visible, setVisible] = useState(false);
+
+    function switchDisplay() {
+        console.log("Check");
+        setVisible(true);
+
+        if(visible == true){
+            setVisible(false);
+        }
+    }
+
     return (
         <>
             <NavBar />
 
-            <img className="img-filter" src={filter} alt="" />
-            
+            <img className="img-filter" src={filter} alt="" onClick={() => switchDisplay()} />
 
-            <CardMeusAnuncios bairro="Vila Mariana" cidade="São Paulo - SP" valor="R$ 1000.00,00" />
+            <div className={visible ? "prime-block-visible" : "prime-block-invisible"} >
+                <div>
+                    <b>Tipo do Aluguel</b><br /><br />
+                    <a>Diária</a> <br /><br />
+                    <a>Semanal</a> <br /><br />
+                    <a>Mensal</a> <br /><br />
+                </div>
+
+            </div>
+
+            <Card bairro="Vila Mariana" cidade="São Paulo - SP" valor="R$ 1000.00,00" />
 
 
 
