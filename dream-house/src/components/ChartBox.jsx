feb2bf1dc42612359ef92e983d7bc9eb2590bc26
Kennedy Martins <kennedy.martins@valemobi.com.br>
2022-05-23 20:59:14 -0300

    Salvando alterações feitas na aula

--- dream-house/src/components/ChartBox.jsx
+++ dream-house/src/components/ChartBox.jsx
@@ -1,29 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import '../css/chartbox.css';
-import {Bar} from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
+import { CategoryScale } from 'chart.js';
+import Chart from 'chart.js/auto';
+import { Api } from "@mui/icons-material";
 
-function ChartBox(){
-    return(
+function ChartBox() {
+    Chart.register(CategoryScale);
+
+
+    const [datasets, setDatasets] = useState([]);
+    const [labels, setLabels] = useState([]);
+
+
+    useEffect(() => {
+
+        // Api.asffsafsa().then(res => {
+        //     setLabels(res.data.labels)
+        //     setDatasets(res.data.data)
+        // })
+        
+        setLabels(['Jun', 'Jul', 'Aug'])
+
+        setDatasets([
+            {
+                label: 'a',
+                data: [5, 6, 7],
+            },
+            {
+                label: 'b',
+                data: [3, 2, 1],
+            },
+        ])
+    }, [])
+
+    return (
         <>
-         
-               <Bar 
+            <Bar
+                datasetIdKey='id'
                 data={{
-                    labels: ['Red', 'Blue'],
-                    datasets:[{
-                        label: '# of votes',
-                        data:[12,19],
-                        
-                    }]
+                    labels: labels,
+                    datasets: datasets
                 }}
-                height={200}
-                width={400}
-                options={{
-                    maintainAspectRatio: false,
-                }}
-               />
-           
+            />
         </>
-    );
+    )
+
 }
 
 export default ChartBox;
--- dream-house/src/components/NavBar.jsx
+++ dream-house/src/components/NavBar.jsx
@@ -67,7 +67,7 @@ function NavBar() {
             </div>
 
             
-            <div className={visible ? "modal-position-invisible" : "modal-position-visible"} id="m1">
+            <div className={visible ? "modal-position-visible" : "modal-position-invisible"} id="m1">
                 <ModalDeConfig />
             </div>
 
