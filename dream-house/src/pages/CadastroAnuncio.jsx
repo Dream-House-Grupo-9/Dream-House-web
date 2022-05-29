2ea9113f833a44df39a6b5407f27438a3064010d
Kennedy Martins <kennedy.martins@valemobi.com.br>
2022-05-28 23:15:01 -0300

    Fix: adição dos campos restantes na cadastro anuncio

--- dream-house/src/pages/CadastroAnuncio.jsx
+++ dream-house/src/pages/CadastroAnuncio.jsx
@@ -79,7 +79,7 @@ function CadastroAnuncio() {
 
                 <div class="input-group">
                     <form action="formulario-cadastro-imove">
-                        <div className="inpt-row">
+                        <div className="inpt-row center-row">
                             <div class="input-box">
                                 <label for="firstname">Descrição</label>
                                 <input type="text" name="firstname" required />
@@ -87,11 +87,11 @@ function CadastroAnuncio() {
 
                             <div class="input-box">
                                 <label for="firstname">Inicio Disponibilidade</label>
-                                <input type="text" name="firstname"  required />
+                                <input type="text" name="firstname" required />
                             </div>
                         </div>
 
-                        <div className="inpt-row">
+                        <div className="inpt-row center-row">
                             <div class="input-box">
                                 <label for="firstname">Fim Disponibilidade</label>
                                 <input type="text" name="firstname" required />
@@ -99,11 +99,11 @@ function CadastroAnuncio() {
 
                             <div class="input-box">
                                 <label for="firstname">Cidade</label>
-                                <input type="text" name="firstname"  required />
+                                <input type="text" name="firstname" required />
                             </div>
                         </div>
 
-                        <div className="inpt-row">
+                        <div className="inpt-row center-row">
                             <div class="input-box">
                                 <label for="firstname">Bairro</label>
                                 <input type="text" name="firstname" required />
@@ -115,7 +115,7 @@ function CadastroAnuncio() {
                             </div>
                         </div>
 
-                        <div className="inpt-row">
+                        <div className="inpt-row center-row">
                             <div class="input-box">
                                 <label for="firstname">Numero</label>
                                 <input type="text" name="firstname" required />
@@ -128,10 +128,91 @@ function CadastroAnuncio() {
 
                         </div>
 
-                        <div className="inpt-row">
-                            <button className="btn-next-process right" onClick={() => navigate("/login")}>Segunda Etapa</button>
-                            <button className="btn-images" type="submit">Inserir Imagens</button>
+                        <div className="inpt-row center-row">
+                            <div class="input-box">
+                                <label for="firstname">Valor Semanal</label>
+                                <input type="text" name="firstname" required />
+                            </div>
+
+                            <div class="input-box">
+                                <label for="firstname">Valor Diaria</label>
+                                <input type="text" name="firstname" required />
+                            </div>
+
                         </div>
+
+                        <div className="inpt-row center-row">
+                            <div class="input-box">
+                                <label for="firstname">Valor Mensal</label>
+                                <input type="text" name="firstname" required />
+                            </div>
+
+                            <div class="input-box">
+                                <label for="firstname">Quantidade de dormitórios</label>
+                                <input type="text" name="firstname" required />
+                            </div>
+
+                        </div>
+
+                        <div className="inpt-row">
+                            <div class="input-box center">
+                                <label for="firstname">Quantidade toalettes</label>
+                                <input type="text" className="input-large" name="firstname" required />
+                            </div>
+                        </div>
+                        <br />
+                   
+                        
+                        <div className="inpt-row ">
+                            <div className="form-content left">
+
+                                <div className="form-column">
+                                    <label className="label-form-imovel-register-details"> Possui garagem? </label><br />
+                                    <div className="row-tick">
+                                        <div className="row-tick-item">
+                                            <label>Sim</label>
+                                            <input className="input-radio-imovel-register" type="radio" />
+                                            <label>Não</label>
+                                            <input className="input-radio-imovel-register" type="radio" />
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="form-column align-right">
+                                    <label className="label-form-imovel-register-details"> Possui area de trabalho? </label><br />
+                                    <div className="row-tick">
+                                        <div className="row-tick-item">
+                                            <label>Sim</label>
+                                            <input className="input-radio-imovel-register" type="radio" />
+                                            <label>Não</label>
+                                            <input className="input-radio-imovel-register" type="radio" />
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="form-column left">
+                                    <label className="label-form-imovel-register-details"> Possui garagem </label><br />
+                                    <div className="row-tick">
+                                        <div className="row-tick-item">
+                                            <label>Sim</label>
+                                            <input className="input-radio-imovel-register" type="radio" />
+                                            <label>Não</label>
+                                            <input className="input-radio-imovel-register" type="radio" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <br />
+
+
+                        <div className="inpt-row">
+                            <button className="btn-images right" type="submit">Inserir Imagens</button>
+                            <button className="btn-next-process" onClick={() => navigate("/login")}>Segunda Etapa</button>
+                        </div>
+
+
+
 
                     </form>
 
