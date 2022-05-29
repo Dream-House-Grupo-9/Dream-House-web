6dd235e0f12045e9f8e5b47fe2a4114090bbe1aa
Kennedy Martins <kennedy.martins@valemobi.com.br>
2022-05-29 01:04:59 -0300

    Fix: remoção de comentarios

--- dream-house/src/pages/CadastroAnuncio.jsx
+++ dream-house/src/pages/CadastroAnuncio.jsx
@@ -11,67 +11,6 @@ function CadastroAnuncio() {
     return (
         <>
             <NavBarCompacta />
-
-            {/* <div className="box">
-                <div className="text-container">
-                    <h4>Cadastre sua locação!</h4>
-                </div>
-
-                <div className="input-container">
-                    <form className="formulario-cadastro-imovel">
-                        <label className="label-form-imovel-register"> <b> Descrição </b> </label><br />
-                        <input className="input-form-imovel-register" type="text" /><br />
-                        <br />
-
-                        <div className="row-cadastro-anuncio">
-                            <div className="row-content">
-                                <label className="label-form-imovel-register"> <b> Inicio Disponibilidade</b> </label><br />
-                                <input className="input-form-imovel-register-default-end left" type="text" />
-
-                            </div>
-
-                            <div className="row-content">
-                                <label className="label-form-imovel-register-end"> <b> Fim Disponibilidade</b> </label><br />
-                                <input className="input-form-imovel-register-default-end" type="text" />
-                            </div>
-                        </div>
-
-                        comentar
-                        <div className="row-especial">
-                            <label className="label-form-imovel-register"> <b> Cidade </b> </label><br />
-                            <input className="input-form-imovel-register-default" type="text" />
-
-                            <label className="label-form-imovel-register-end"> <b> Bairro </b> </label><br />
-                            <input className="input-form-imovel-register-default-end" type="text" />
-                        </div>
-
-                        <div className="row-especial">
-                            <label className="label-form-imovel-register"> <b> Logradouro </b> </label><br />
-                            <input className="input-form-imovel-register-default" type="text" />
-
-                            <label className="label-form-imovel-register-end"> <b> Número </b> </label><br />
-                            <input className="input-form-imovel-register-default-end" type="text" />
-                        </div>
-
-                        <div className="row-especial">
-                            <label className="label-form-imovel-register"> <b> Nome do Proprietário </b> </label><br />
-                            <input className="input-form-imovel-register-default" type="text" />
-
-                            <button className="btn-images" type="submit">Inserir Imagens</button>
-                        </div>
-                        cabo
-
-                        <br /><br />
-
-                        
-                        <button onClick={() => navigate("/cadastro-anuncio-detalhes")} className="btn-segunda-etapa">Segunda Etapa</button>
-                        
-
-                    </form>
-                </div>
-
-            </div> */}
-
             <div className="form">
                 <div className="text-container">
                     <h4>Cadastre sua locação!</h4>
