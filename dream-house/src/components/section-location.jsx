69abf1875374d5c9c0ce806dfc89b15a06fd3f8d
Kennedy Martins <kennedy.martins@valemobi.com.br>
2022-05-29 12:15:05 -0300

    Feature: adição dos campos restantes

--- dream-house/src/components/section-location.jsx
+++ dream-house/src/components/section-location.jsx
@@ -15,27 +15,67 @@ function SectionLocation() {
                     </div>
                     <hr className="divisoria vertical" />
                     <div className="location-info">
-                        <h2>Apartamento Vila Mariana - SP, São Paulo</h2>
-                        <br />
-                        <h3><b>Endereço:</b>Rua Afonso Celso, 190 - vila mariana, SP</h3>
-                        <br />
-                        <h3><b>CEP: </b>04119-002</h3>
-                        <br />
-                        <h3><b>Valor Diária: R$</b>350,00</h3>
-                        <br />
-                        <h3><b>Valor Semanal: R$</b>350,00</h3>
-                        <br />
-                        <h3><b>Valor Mensal: R$</b>350,00</h3>
-                        <br />
-                        <div className="profile-info">
-                            <img src={Profile} alt="Whatsapp" className="section-icons" />
-                            <h4>Henri Cauã</h4>
+                        <div className="info-space">
+                            <h2>Apartamento Vila Mariana</h2>
                         </div>
-                        <br />
+
+                        <div className="info-space">
+                            <h4><b>Categoria: </b>Apartamento</h4>
+                        </div>
+
+                        <div className="info-space">
+                            <h4><b>Cidade: </b>São Paulo</h4>
+                        </div>
+                        
+                        <div className="info-space">
+                            <h4><b>Bairro: </b>Vila Mariana</h4>
+
+                        </div>
+
+                        <div className="info-space">
+                            <h4><b>Logradouro: </b>Rua Afonso Celso</h4>
+                        </div>
+
+                        <div className="info-space">
+
+                            <h4><b>Numero: </b>190</h4>
+                        </div>
+
+                        <div className="info-space">
+
+                            <h4><b>Valor Diária: R$</b>350,00</h4>
+                        </div>
+
+                        <div className="info-space">
+
+                            <h4><b>Valor Semanal: R$</b>350,00</h4>
+                        </div>
+                        <div className="info-space">
+
+                            <h4><b>Valor Mensal: R$</b>350,00</h4>
+                        </div>
+
+                        <div className="info-space">
+
+                            <h4><b>Tipo Ativo: </b>Mensal</h4>
+                        </div>                        
+
+                        <div className="info-space">
+
+                            <div className="profile-info">
+                                <img src={Profile} alt="Whatsapp" className="section-icons" />
+                                <h4>Henri Cauã</h4>
+                            </div>
+                        </div>
+
+                        <div className="info-space">
+
+                        </div>
                         <div className="profile-info">
                             <img src={WhatsApp} alt="Whatsapp" className="section-icons" />
                             <h4>94242-8922</h4>
                         </div>
+                        <br />
                     </div>
                     <br />
                 </div>
