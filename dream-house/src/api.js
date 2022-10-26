418734a444f64ae37c05db74f47b0be3ba1c87de
Henri Cauã <79324149+Henri011@users.noreply.github.com>
2022-10-26 20:57:37 -0300

    Update api.js
    Alterando o consumo da api para o IP elastico da aws

--- dream-house/src/api.js
+++ dream-house/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const Api = axios.create({
 
-    baseURL: "http://localhost:8080"
+    baseURL: "http://44.209.70.78:8002"
     // headers: {
     //     'Access-Control-Allow-Origin': '*',
     //     "Content-Type": "application/json;charset=UTF-8"
